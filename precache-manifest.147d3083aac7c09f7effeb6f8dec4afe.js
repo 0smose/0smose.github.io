5166a3d5121f3d147b61ad498db1a98bb453c07c
Robin Dercole <robin.dercole@gmail.com>
2020-09-12 19:11:48 +0200

    Updates

---
+++ precache-manifest.147d3083aac7c09f7effeb6f8dec4afe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdba3f46fa0cf5f2f096083c9b7befd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2242ff7bb80ec6c01a05",
+    "url": "./static/css/main.b53f1039.chunk.css"
+  },
+  {
+    "revision": "64fc8e1b93fdc1ee7955",
+    "url": "./static/js/2.91bc9b86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.91bc9b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2242ff7bb80ec6c01a05",
+    "url": "./static/js/main.b5827f4a.chunk.js"
+  },
+  {
+    "revision": "16e7070e211459df62f2",
+    "url": "./static/js/runtime-main.af82dc02.js"
+  },
+  {
+    "revision": "ff6e3f50072308ce1291f4a3b52f38f2",
+    "url": "./static/media/eventbrite.ff6e3f50.png"
+  },
+  {
+    "revision": "9749181d531aa5add4ff78a7e910aae1",
+    "url": "./static/media/fako-fashion.9749181d.png"
+  },
+  {
+    "revision": "9ef94b18b6dd0fb2ef0ef84042cbdb72",
+    "url": "./static/media/merry.9ef94b18.png"
+  },
+  {
+    "revision": "8bb75b65edbe427ad1fec7ea5f1af3c1",
+    "url": "./static/media/progame.8bb75b65.png"
+  },
+  {
+    "revision": "d2eb2f2529b2c1afc045c4a1e928c45c",
+    "url": "./static/media/sharesquare.d2eb2f25.png"
+  }
+]);
